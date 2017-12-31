8999fbeac7c3cfee572dcc482434ba7bd93085de
Charles James <bigmuug@gmail.com>
2017-12-31 02:18:12 -0500

    test1

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,10 @@
+print('Hello World! Hello world\n Hello world \r')
+print(", ".join(["sharks", "crustaceans"]).lower())
+print(",".join(["sharks"]))
+weekdays = "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+
+print(''.join(weekdays))
+
+num = 6 * 6 * 6
+
+print(num)
