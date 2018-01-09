bbc5bf8896e65d669c0238088416db727ce94fc6
Charles James <bigmuug@gmail.com>
2018-01-08 23:44:48 -0500

    S

--- Data Type Function Examples/ifstatements.py
+++ Data Type Function Examples/ifstatements.py
@@ -1,6 +1,5 @@
 name = input('Please enter your name: ')
-age = input("How old are you , {0}? ".format(name))
+age = int(input("How old are you , {0}? ".format(name)), 16)
 print(age)
-
 if age == 12:
     print("hehe xd")
